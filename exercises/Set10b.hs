b66706501545f0d0146cf704b22ef3dbc3277a94
jarm111 <44726773+jarm111@users.noreply.github.com>
2026-09-14 15:19:02 +0300

    Set10b done

--- exercises/Set10b.hs
+++ exercises/Set10b.hs
@@ -22,7 +22,8 @@ import Mooc.Todo
 --   False ||| undefined ==> an error!
 
 (|||) :: Bool -> Bool -> Bool
-x ||| y = todo
+x ||| True = True
+x ||| _ = x 
 
 ------------------------------------------------------------------------------
 -- Ex 2: Define the function boolLength, that returns the length of a
@@ -36,7 +37,9 @@ x ||| y = todo
 --   length [False,undefined] ==> 2
 
 boolLength :: [Bool] -> Int
-boolLength xs = todo
+boolLength [] = 0
+boolLength (True:xs) = 1 + boolLength xs
+boolLength (_   :xs) = 1 + boolLength xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Define the function validate which, given a predicate and a
@@ -50,7 +53,9 @@ boolLength xs = todo
 --   validate (\x -> undefined) 3  ==>  an error!
 
 validate :: (a -> Bool) -> a -> a
-validate predicate value = todo
+validate predicate value = go (predicate value) value
+  where go True val = val
+        go _    val = val
 
 ------------------------------------------------------------------------------
 -- Ex 4: Even though we can't implement the generic seq function
@@ -84,10 +89,13 @@ class MySeq a where
   myseq :: a -> b -> b
 
 instance MySeq Bool where
-  myseq = todo
+  myseq True val = val
+  myseq _    val = val
 
 instance MySeq Int where
-  myseq = todo
+  myseq 0 val = val
+  myseq _ val = val
 
 instance MySeq [a] where
-  myseq = todo
+  myseq [] val     = val
+  myseq _  val     = val
